e29aa22f2c7ea39cf3481345148e523a36b8f5a9
Volker Nauruhn <684302+razorness@users.noreply.github.com>
2021-08-04 11:38:27 +0200

    inconsistent event context demo

---
+++ src/components/Comp.vue
@@ -0,0 +1,10 @@
+<template>
+	<button @click="$emit('custom:click', 'clicked')">CLICK ME</button>
+</template>
+
+<script>
+	export default {
+		name : 'Comp',
+		emits: [ 'custom:click' ]
+	}
+</script>
